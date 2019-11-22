27e541ee8730d8e73b3c4c78bba53311172c2bc3
awalwis17 <alexandre.walwis@student.vinci.be>
2019-11-22 09:58:37 +0100

    email

--- projet.sql
+++ projet.sql
@@ -45,7 +45,7 @@ CREATE TABLE projet.concerts(
 
 CREATE TABLE projet.clients(
 	id_client SERIAL PRIMARY KEY,
-	email VARCHAR(100) UNIQUE NOT NULL CHECK(email SIMILAR TO '([0-9a-zA-Z]*.[0-9a-zA-Z]*)@([0-9a-zA-Z]*.[0-9a-zA-Z]*)'),
+	email VARCHAR(100) UNIQUE NOT NULL CHECK(email SIMILAR TO '[0-9a-zA-Z]*(.[0-9a-zA-Z]*)*@[0-9a-zA-Z]*(.[0-9a-zA-Z]*)*'),
 	nom_utilisateur VARCHAR(100) UNIQUE NOT NULL CHECK(nom_utilisateur <> ''),
 	mot_de_passe VARCHAR(100) NOT NULL CHECK(mot_de_passe <> ''),
 	sel VARCHAR(255) NOT NULL
